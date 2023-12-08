4a47754106b7ad681ed89f59596c9afc76ad5f91
Lucas-Debiais <lucas.debiais11@gmail.com>
2023-12-08 15:31:31 +0100

    feat: add admin dashboard stats

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Party;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,12 +13,33 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class AdminController extends AbstractController
 {
     #[IsGranted('ROLE_ADMIN')]
-    #[Route('/admin/', name: 'app_admin')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(EntityManagerInterface $em): Response
     {
         $users = $em->getRepository(User::class)->findAll();
+        $parties = $em->getRepository(Party::class)->findAll();
+
+        $countNull = 0;
+
+        foreach ($parties as $partie) {
+            if ($partie->getWhoWin() === null) {
+                $countNull += 1;
+            }
+        }
 
         return $this->render('admin/index.html.twig', [
+            'users' => $users,
+            'parties' => $parties,
+            'countNull' => $countNull,
+        ]);
+    }
+
+    #[Route('/admin/user', name: 'app_admin_user')]
+    public function user(EntityManagerInterface $em): Response
+    {
+        $users = $em->getRepository(User::class)->findAll();
+
+        return $this->render('admin/user.html.twig', [
             'users' => $users,
         ]);
     }
@@ -43,7 +65,6 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/user/{id}/delete', name: 'app_admin_delete')]
-
     public function deleteUser(User $user, EntityManagerInterface $em): Response
     {
         $em->remove($user);
